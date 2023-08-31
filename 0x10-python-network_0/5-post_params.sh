d3973da82c40dd9a2d32b33596d542cf999da1d1
OdochHerbert <121699258+OdochHerbert@users.noreply.github.com>
2023-08-31 19:42:11 +0300

    angelslikeyou

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This data can be used by the server to process the request and generate a response
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
-
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
